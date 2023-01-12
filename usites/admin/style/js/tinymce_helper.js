24ea756a597d8d60cfd2df6a6f8d8f9497ffded9
kobkob2018 <35145209+kobkob2018@users.noreply.github.com>
2023-01-12 22:43:25 +0200

    טייני מושלם

--- usites/admin/style/js/tinymce_helper.js
+++ usites/admin/style/js/tinymce_helper.js
@@ -19,15 +19,15 @@ function init_tinymce(selector_identifier,media_uploader_url, media_library_url)
     global_media_library_url = media_library_url;
     tinymce.init({
         selector: selector_identifier,
-        plugins: 'image code textcolor link colorpicker lists codesample advlist autosave contextmenu emoticons fullscreen help hr insertdatetime nonbreaking paste preview' ,
-        toolbar: 'undo redo | image code link | numlist bullist| forecolor backcolor | paste preview |nonbreaking codesample restoredraft emoticons | fullscreen help hr insertdatetime',
+        plugins: 'image code textcolor link colorpicker lists codesample advlist autosave contextmenu emoticons fullscreen help hr insertdatetime nonbreaking paste preview print searchreplace table' ,
+        toolbar: ['undo redo | image code link hr insertdatetime | numlist bullist table',
+            'bold italic underline blocks | forecolor backcolor fontsize fontfamily styles | restoredraft paste preview print | nonbreaking codesample emoticons | fullscreen help'],
         contextmenu: "link image inserttable | cell row column deletetable",
         directionality : "rtl",
         insertdatetime_dateformat: "%Y-%m-%d",
         /* without images_upload_url set, Upload tab won't show up*/
         images_upload_url: media_uploader_url,
         content_style: 'img {max-width: 100%; height:auto;}',
-    
         file_picker_callback: function(callback, value, meta) {
             return tiny_mce_callback_handler.init(callback, value, meta);
         }
